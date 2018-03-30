6a58ad2a0de4d1419b4c199000f1186eee8ec0b8
chasanem <chasanem@127.0.0.1>
2018-03-30 16:35:21 +0200

    jeu d'essai

--- src/test/Test.java
+++ src/test/Test.java
@@ -24,5 +24,40 @@ public class Test {
 			System.out.println();
 		}
 		
+		
+		/*Création de la base de données à metttre en oeuvre*/
+		/*PRODUIT*/
+		Schema sc1 = new Schema(new Attribut("DENOMINATION", new STRING("string")), new Attribut("ENERGIE", new STRING("int")), new Attribut("PROTEINE", new STRING("double")));
+		StateFulInMemory relation1 = new StateFulInMemory("PRODUIT",sc1);
+		relation.add(new Tuple("Epautre",1340, 11.5));
+		relation.add(new Tuple("Orge",1430, 11.0));
+		relation.add(new Tuple("Avoine",1530, 12.5));
+		relation.add(new Tuple("Millet",1510, 10.5));
+		relation.add(new Tuple("Mais",1498, 9.0));
+		relation.add(new Tuple("Riz",1492, 7.5));
+		relation.add(new Tuple("Segle",1323, 8.8));
+		relation.add(new Tuple("Ble",1342, 11.5));
+
+		for(_Tuple t : relation1){
+			for(Object o : t){
+				System.out.print(o+" ");
+			}
+			System.out.println();
+		}
+		
+		/*PRODUCTEUR*/
+		Schema sc2 = new Schema(new Attribut("ID_PRODUCTEUR", new STRING("int")), new Attribut("NOM", new STRING("string")), new Attribut("PRENOM", new STRING("string")));
+		StateFulInMemory relation2 = new StateFulInMemory("PRODUCTEUR",sc2);
+		relation.add(new Tuple(1,"BERNARD", "ALAIN"));
+		relation.add(new Tuple(2,"PERRIER", "CHARLES"));
+		relation.add(new Tuple(3,"LABBE", "CAROLINE"));
+
+		for(_Tuple t : relation1){
+			for(Object o : t){
+				System.out.print(o+" ");
+			}
+			System.out.println();
+		}
+		
 	}
 }
