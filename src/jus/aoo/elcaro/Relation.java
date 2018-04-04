ddfe5119b0b95329ebfcc56582d07f5b34f2cab3
Antoine Carrier <carriean@f203-06.e-im2ag.ujf-grenoble.fr>
2018-04-04 09:38:37 +0200

    debut cles dans statfulinmemory

--- src/jus/aoo/elcaro/Relation.java
+++ src/jus/aoo/elcaro/Relation.java
@@ -5,6 +5,5 @@ public abstract class Relation implements Iterable<_Tuple> {
 	private final Schema schema;
 	public String getNom() {return nom;}
 	public Schema getSchema() {return schema;}
-	protected Relation(String nom, Schema schema){ this.nom=nom; this.schema= schema;}
-	
+	protected Relation(String nom, Schema schema){ this.nom=nom; this.schema= schema;}	
 }
--- src/jus/aoo/elcaro/StateFulInMemory.java
+++ src/jus/aoo/elcaro/StateFulInMemory.java
@@ -1,13 +1,23 @@
 package jus.aoo.elcaro;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Iterator;
 
 public class StateFulInMemory extends StateFull {
+	/**
+	 * Représentation des tuples d'une relation statefull
+	 */
 	private ArrayList<_Tuple> tuples;
+	/**
+	 * Représentation des clés d'une relation statefull
+	 */
+	private HashMap<_Attribut, _Tuple> keys;
+	
 	public StateFulInMemory(String nom, Schema schema) {
 		super(nom, schema);
 		tuples = new ArrayList<>();
+		keys = new HashMap<>();
 	}
 
 	@Override public Iterator<_Tuple> iterator() {
@@ -29,4 +39,13 @@ public class StateFulInMemory extends StateFull {
 	@Override public void remove(int index, _Tuple tuple) {
 		tuples.remove(index);		
 	}
+	
+	
+	
 }
+
+/* 	Pour la gestion des clés, faire hashmap avec Key/Tuple
+ 	Si clé int, relation d'ordre
+*/
+ 
+
